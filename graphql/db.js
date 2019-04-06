6c3ee1b36e2ed17b5ce41c380b7df21646c3df58
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-04-06 14:28:02 +0900

    1.10 Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,4 +1,4 @@
-export const movies = [
+export let movies = [
   {
     id: 0,
     name: "Star Wars - The new one",
@@ -28,16 +28,6 @@ export const getById = id => {
   return filteredPeople[0];
 };
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movie = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
 export const addMovie = (name, score) => {
   const newMovie = {
     id: `${movies.length + 1}`,
@@ -46,4 +36,14 @@ export const addMovie = (name, score) => {
   };
   movies.push(newMovie);
   return newMovie;
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
